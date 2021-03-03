7831ca86758d1072afd4f1d71cfc6f97165a7bcf
ethanmmay <ethaniel.may@gmail.com>
2021-03-03 16:07:31 -0700

    Many-to-Many (Species & Planets) working

--- server/services/PlanetSpeciesService.js
+++ server/services/PlanetSpeciesService.js
@@ -3,7 +3,7 @@ import { dbContext } from "../db/DbContext";
 class PlanetSpeciesService {
 
     async find(query = {}) {
-        return await dbContext.PlanetSpecies.find(query)
+        return await dbContext.PlanetSpecies.find(query).populate("species planet")
     }
 
     async create(data) {
